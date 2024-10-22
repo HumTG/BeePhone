87b6c5b59fa2bd62301844da28aedfe62b431b8a
Hồ Minh <zerotoone288@gmail.com>
2024-10-22 22:42:17 +0700

    UI login

--- src/main/java/org/example/beephone/controller/thymeleaf/customer/IndexController.java
+++ src/main/java/org/example/beephone/controller/thymeleaf/customer/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "customers/index";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "customers/login";
+    }
 }
