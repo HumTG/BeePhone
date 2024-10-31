549834d6ae118d277b35ab221e9ccff2f925f527
Tedomi2710 <duc271004@gmail.com>
2024-10-31 23:19:13 +0700

    fix bug

--- src/main/resources/static/admin/js/KhachHangController.js
+++ src/main/resources/static/admin/js/KhachHangController.js
@@ -1,17 +1,17 @@
 var app = angular.module('KhachHangApp', []);
 
-app.controller('KhachHangController', function($scope, $http, $window) {
+app.controller('KhachHangController', function ($scope, $http, $window) {
     var host = "http://localhost:8080/rest/khach-hang";
     $scope.kh = []; // Khởi tạo danh sách khách hàng
     $scope.newCustomer = {}; // Dữ liệu khách hàng mới
 
     // Hàm để lấy danh sách khách hàng
-    $scope.getData = function() {
+    $scope.getData = function () {
         $http.get(host)
-            .then(function(response) {
+            .then(function (response) {
                 $scope.kh = response.data.content; // Lưu danh sách khách hàng vào scope
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.error("Lỗi khi tải dữ liệu:", error);
             });
     };
@@ -20,56 +20,52 @@ app.controller('KhachHangController', function($scope, $http, $window) {
     $scope.getData();
 
     // Mở form thêm mới khách hàng
-    $scope.openAddForm = function() {
+    $scope.openAddForm = function () {
         document.getElementById("addCustomerModal").style.display = "flex";
     };
 
     // Đóng form thêm mới khách hàng
-    $scope.closeAddForm = function() {
+    $scope.closeAddForm = function () {
         $('#addCustomerModal').modal('hide'); // Đóng modal đúng ID
     };
 
     // Hàm thêm mới khách hàng
-    $scope.addCustomer = function() {
+    $scope.addCustomer = function () {
         console.log("Dữ liệu khách hàng mới:", $scope.newCustomer); // Kiểm tra dữ liệu trước khi gửi
         $http.post(host, $scope.newCustomer) // Sử dụng biến host
-            .then(function(response) {
+            .then(function (response) {
                 console.log("Khách hàng mới đã được thêm:", response.data);
                 $scope.getData(); // Cập nhật danh sách khách hàng sau khi thêm
                 $scope.closeAddForm(); // Đóng form sau khi thêm thành công
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.error("Lỗi khi thêm khách hàng mới:", error);
                 alert("Đã xảy ra lỗi khi thêm khách hàng.");
             });
     };
 
     // Mở trang thêm mới khách hàng
-    $scope.openAddCustomerPage = function() {
+    $scope.openAddCustomerPage = function () {
         $window.location.href = "/admin/create-khach-hang.html";
     };
 
     // Hàm mở modal và hiển thị thông tin chi tiết
-    $scope.openDetail = function(id) {
+    $scope.openDetail = function (id) {
         var hostKH = `${host}/${id}`;
         $http.get(hostKH)
-            .then(function(response) {
+            .then(function (response) {
                 $scope.khct = response.data;
-
                 // Chuyển đổi giới tính
                 $scope.khct.gioi_tinh_display = $scope.khct.gioi_tinh === 0 ? 'Nam' : 'Nữ';
-
                 // Kiểm tra và chuyển đổi 'ngay_sinh' thành kiểu Date
                 if ($scope.khct.ngay_sinh) {
                     $scope.khct.ngay_sinh = new Date($scope.khct.ngay_sinh);
                 }
-
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.error('Error fetching data:', error);
             });
-
-        var detailModal = new bootstrap.Modal(document.getElementById('detailModal'), { keyboard: false });
+        var detailModal = new bootstrap.Modal(document.getElementById('detailModal'), {keyboard: false});
         detailModal.show();
     };
 });
