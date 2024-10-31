e4e7420a64697dc04169233de783ffb313ec98ee
Hồ Minh <zerotoone288@gmail.com>
2024-11-01 00:58:59 +0700

    add màn giảm giá :)))

--- src/main/resources/static/admin/js/GiamGiaController.js
+++ src/main/resources/static/admin/js/GiamGiaController.js
@@ -4,6 +4,16 @@ var host = "http://localhost:8080/rest/giam-gia";
 app.controller('GiamGiaController',function ($scope,$http){
     console.log("đây là giảm giá controller")
     $scope.currentPage = 0;
+    $scope.newGiamGia = {
+        id : null,
+        ma_giam_gia : "GG100",
+        ten : "",
+        gia_tri : 0,
+        ngay_bat_dau : "",
+        ngay_ket_thuc : "",
+        trang_thai : 1
+    }
+
 
     ///cập nhật trạng thái khi đã hết hạn
     // $scope.capNhatTrangThai = function (){
@@ -31,11 +41,58 @@ app.controller('GiamGiaController',function ($scope,$http){
 
     $scope.getData($scope.currentPage)
 
+    ///đổi page
     $scope.changePage = function(page) {
         if (page >= 0 && page < $scope.totalPages) {
             $scope.currentPage = page;
             $scope.getData($scope.currentPage);
         }
     };
+
+    //Open modal
+    $scope.openModalAdd = function (){
+        var addModal = new bootstrap.Modal(document.getElementById('addGiamGiaModal')); // Sử dụng Bootstrap Modal
+        addModal.show(); // Hiển thị modal
+    }
+
+    //CLEAR data
+    $scope.clear = function (){
+        $scope.newGiamGia.id = null,
+        $scope.newGiamGia.ma_giam_gia = "",
+        $scope.newGiamGia.ten = "",
+        $scope.newGiamGia.gia_tri = 0,
+        $scope.newGiamGia.ngay_bat_dau = "",
+        $scope.newGiamGia.ngay_ket_thuc = ""
+    }
+
+    ///add dữ liệu
+    $scope.addGiamGia = function (){
+        var giam_gia = {
+            id : null,
+            ma_giam_gia : $scope.newGiamGia.ma_giam_gia,
+            ten : $scope.newGiamGia.ten,
+            gia_tri : Number($scope.newGiamGia.gia_tri),
+            ngay_bat_dau : new Date($scope.newGiamGia.ngay_bat_dau),
+            ngay_ket_thuc : new Date($scope.newGiamGia.ngay_ket_thuc),
+            trang_thai : 1
+        }
+        $http.post('http://localhost:8080/rest/giam-gia', giam_gia)
+            .then(function (respone){
+                console.log(giam_gia)
+                var modalElement = document.getElementById('addGiamGiaModal');
+                var addModal = bootstrap.Modal.getInstance(modalElement);
+                addModal.hide(); // đóng modal
+                $scope.getData();
+                $scope.clear();
+                toastr.success('Thành công', 'OK')
+            }) .catch(function(error) {
+            console.error('Lỗi :', error);
+            toastr.error('Có lỗi xảy ra!', 'Error', {
+                closeButton: true,
+                progressBar: true,
+                timeOut: 3000
+            });
+        });
+    }
 
 })
